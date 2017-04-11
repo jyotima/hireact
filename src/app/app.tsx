1fa8575cb3e55784140f8863103bd4fd11c9338e
Jyoti Ranjan Mahapatra <jyotima@microsoft.com>
2017-04-10 22:42:43 -0700

    prop2

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 interface ICustomProps {
   txt: string;
+  cat: string;
 }
 export class App extends React.Component<ICustomProps, void> {
   public render(): JSX.Element {
     let txt = this.props.txt;
-    return <div><h1>{txt}</h1></div>;
+    let cat = this.props.cat;
+    return <div><h1>{txt}</h1><h2>{cat}</h2></div>;
   }
 }
 export default App;
--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,4 +2,4 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './app'
 
-ReactDOM.render(<App txt="this is the prop text1"/>, document.getElementById('root'));
+ReactDOM.render(<App txt="this is the prop text1" cat="another"/>, document.getElementById('root'));
